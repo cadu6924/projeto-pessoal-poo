9ef2f821a1b3c8f7a277bcc374e5fed826bdcb30
Eduardo Araújo <151750107+cadu6924@users.noreply.github.com>
2024-12-03 16:05:11 -0300

    Add files via upload

---
+++ src/components/card/App.jsx
@@ -0,0 +1,7 @@
+import Card from './index';
+export default function App (){
+    return <div>
+        <Card/>
+
+    </div>
+}
---
+++ src/components/card/index.jsx
@@ -0,0 +1,13 @@
+export default function App (){
+    return <div>
+        <img class = "deadpool" src="R.png" />
+        <div class = "blocos">
+        <h1>Pôster: Deadpool(2016)</h1>
+        <p> <i>O primeiro filme do Deadpool, lançado em 2016, é uma adaptação
+             dos quadrinhos da Marvel Comics e é conhecido por seu humor irreverente e ação intensa </i></p>
+        </div>
+
+        <a href="https://youtu.be/ByGMCNbo5JU">assistir trailer</a>
+    </div>
+}
+
